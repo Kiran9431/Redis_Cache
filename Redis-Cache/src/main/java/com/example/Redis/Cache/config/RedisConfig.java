5896695f195110be25eed2f0986d6d09706b13a4
Kiran9431 <138762229+Kiran9431@users.noreply.github.com>
2023-08-29 06:02:04 +0400

    Endpoints

--- Redis-Cache/src/main/java/com/example/Redis/Cache/config/RedisConfig.java
+++ Redis-Cache/src/main/java/com/example/Redis/Cache/config/RedisConfig.java
@@ -3,7 +3,6 @@ package com.example.Redis.Cache.config;
 import com.example.Redis.Cache.Binding.Employee;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnection;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -20,7 +19,6 @@ public class RedisConfig {
     public RedisTemplate<String, Employee> redisTemplate(){
         RedisTemplate<String , Employee> reditem = new RedisTemplate<>();
         reditem.setConnectionFactory(jediscon());
-
         return reditem;
     }
 
